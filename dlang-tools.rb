c12c28b307856fcd495f3213f81db69976f59a83
kimon <kimon.kontosis@gmail.com>
2018-07-21 17:31:33 +0300

    make the package dlang-tools only for rdmd

--- dlang-tools.rb
+++ dlang-tools.rb
@@ -1,16 +1,20 @@
-class DlangTools < Formula
-  desc "D programming language tools"
-  homepage "https://dlang.org/"
+class Rdmd < Formula
+  desc "D programming language tool rdmd"
+  homepage "https://dlang.org/rdmd.html"
   head "https://github.com/dlang/tools.git"
   url "https://github.com/dlang/tools/archive/master.tar.gz"
   version "1.0"
+
+  # The rdmd binary already exists in dmd Cellar but it is not linked.
+  # An easier approach would be to link it
+  # Here, experimenting with a full build
 
   depends_on "automake"
   depends_on "dmd"
   depends_on "dub"
 
   def install
-    system "make", "-f", "posix.mak", "all", "test_tests_extractor", "DMD=dmd", "INSTALL_DIR=#{prefix}"
+    system "make", "-f", "posix.mak", "rdmd", "DMD=dmd", "INSTALL_DIR=#{prefix}"
     system "make", "-f", "posix.mak", "install", "DMD=dmd", "INSTALL_DIR=#{prefix}"
 
     end
