ec012d39a5b42a2d7ce245e8332d01b6106c6d73
kimon <kimon.kontosis@gmail.com>
2018-07-21 17:01:32 +0300

    fix system add comas

--- dlang-tools.rb
+++ dlang-tools.rb
@@ -10,14 +10,14 @@ class DlangTools < Formula
   depends_on "dub"
 
   def install
-    system "make" "-f" "posix.mak" "all" "INSTALL_DIR=#{prefix}"
-    system "make" "-f" "posix.mak" "install" "INSTALL_DIR=#{prefix}"
+    system "make", "-f", "posix.mak", "all", "INSTALL_DIR=#{prefix}"
+    system "make", "-f", "posix.mak", "install", "INSTALL_DIR=#{prefix}"
 
     end
 
     test do
       system "rdmd"
-      system "which" "rdmd"
+      system "which", "rdmd"
 
     end
 end
