7313c40de5ffd1f8584bdbb85f710978328ad6c3
kimon <kimon.kontosis@gmail.com>
2018-07-24 14:20:07 +0300

    check if rdmd is already linked

--- rdmd.rb
+++ rdmd.rb
@@ -1,22 +1,30 @@
 class Rdmd < Formula
-  desc "D programming language tool rdmd symlink"
+  desc "D programming language tool rdmd"
   homepage "https://dlang.org/rdmd.html"
   head "https://github.com/dlang/tools.git"
   url "https://github.com/dlang/tools/archive/master.tar.gz"
   version "1.0"
 
-  # The rdmd binary already exists in dmd Cellar but it is not linked in some cases.
-  # Additionally installs dub
+  # The rdmd binary already exists in dmd Cellar but it is not linked.
+  # An easier approach would be to link it
+  # Here, experimenting with a full build
 
   depends_on "automake"
   depends_on "dmd"
   depends_on "dub"
 
   def install
+    system "make", "-f", "posix.mak", "rdmd", "DMD=dmd", "INSTALL_DIR=#{prefix}"
 
-    # system "bash", "-c", "test -f /usr/local/bin/rdmd || ln -s /usr/local/Cellar/dmd/2.*/bin/rdmd /usr/local/bin/rdmd"
-    system "mkdir", "bin"
-    system "bash", "-c", "test -f /usr/local/bin/rdmd || cp /usr/local/Cellar/dmd/2.*/bin/rdmd bin/"
+    # make install, will make everything but we want just rdmd
+    # system "make", "-f", "posix.mak", "install", "DMD=dmd", "INSTALL_DIR=#{prefix}"
+
+    system "bash", "-c", "cp generated/*/*/rdmd ."
+    system "mkdir", "-p", "#{prefix}/bin"
+    system "cp", "./rdmd", "#{prefix}/bin"
+
+    # do not use it if it is already linked
+    system "bash", "-c", "test -f /usr/local/bin/rdmd && rm #{prefix}/bin/ || echo ok"
 
   end
 
