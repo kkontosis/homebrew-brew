d81206f9d43cf07c7abb12ba4baac1af23a7fba4
kimon <kimon.kontosis@gmail.com>
2018-07-26 15:25:30 +0300

    add dub-config

---
+++ dub-config.rb
@@ -0,0 +1,18 @@
+class DubConfig < Formula
+  desc "D language helper build utility that extracts library include paths from the dub package manager configuration"
+  homepage "https://github.com/kkontosis/dub-config"
+  head "https://github.com/kkontosis/dub-config.git"
+  url "https://github.com/kkontosis/dub-config/archive/master.tar.gz"
+  version "1.0"
+
+  depends_on "dmd"
+  depends_on "dub"
+
+  def install
+    system "dub", "build"
+
+    system "mkdir", "-p", "#{prefix}/bin"
+    system "cp", "./dub-config", "#{prefix}/bin"
+  end
+end
+
